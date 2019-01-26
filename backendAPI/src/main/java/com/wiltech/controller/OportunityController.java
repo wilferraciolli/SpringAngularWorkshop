1d3446b8cbbe7a983eeed0c58cb53a1bef0aa4e1
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-01-26 22:31:06 +0000

    Added create and update methods

--- backendAPI/src/main/java/com/wiltech/controller/OportunityController.java
+++ backendAPI/src/main/java/com/wiltech/controller/OportunityController.java
@@ -3,11 +3,14 @@ package com.wiltech.controller;
 import com.wiltech.model.Oportunity;
 import com.wiltech.repository.OportunityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * The type Oportunity controller.
@@ -28,5 +31,58 @@ public class OportunityController {
     public List<Oportunity> getAll() {
 
         return oportunityRepository.findAll();
+    }
+
+    /**
+     * Find oportunity.
+     *
+     * @param id the id
+     * @return the oportunity
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Oportunity> find(@PathVariable("id") final Long id) {
+
+        final Optional<Oportunity> oportunity = oportunityRepository.findById(id);
+
+        if (oportunity.isPresent()) {
+            return ResponseEntity.ok(oportunity.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Update response entity.
+     *
+     * @param id      the id
+     * @param payload the payload
+     * @return the response entity
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Oportunity> update(@PathVariable("id") final Long id, @Valid @RequestBody final Oportunity payload) {
+
+        final Oportunity oportunity = oportunityRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Oportunity not found for given id."));
+
+        return ResponseEntity.ok(oportunityRepository.save(oportunity));
+    }
+
+    /**
+     * Create oportunity oportunity.
+     *
+     * @param payload the payload
+     * @return the oportunity
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Oportunity createOportunity(@Valid @RequestBody final Oportunity payload) {
+
+        final Optional<Oportunity> existentOportunity = oportunityRepository.findByDescriptionAndProspectName(payload.getDescription(), payload.getProspectName());
+
+        if (existentOportunity.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Oportunities with this description and prospect name already exists.");
+        }
+
+        return oportunityRepository.save(payload);
     }
 }
--- backendAPI/src/main/java/com/wiltech/repository/OportunityRepository.java
+++ backendAPI/src/main/java/com/wiltech/repository/OportunityRepository.java
@@ -3,8 +3,21 @@ package com.wiltech.repository;
 import com.wiltech.model.Oportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * The interface Oportunity repository.
  */
 public interface OportunityRepository extends JpaRepository<Oportunity, Long> {
+
+    /**
+     * Find by description and prospect name optional.
+     * Spring will understand that the query is to find by Description and ProspectName as theses are the actual bean name.
+     * Therefore no query is needed to be created.
+     *
+     * @param description  the description
+     * @param prospectName the prospect name
+     * @return the optional
+     */
+    Optional<Oportunity> findByDescriptionAndProspectName(String description, String prospectName);
 }
